c6b17a5ad8b2cf6446ff07eb6c1cd809eb7d37b6
Ludivine <sagnol.ludivine@gmail.com>
2022-11-08 12:28:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography>Hello Test</Typography>
+    </div>
+  );
+}
+
+export default App;
